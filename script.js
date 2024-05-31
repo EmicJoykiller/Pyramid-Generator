7ef206afbbe8e196d477d12bf3f533020dcb1ed2
nikan <nikanemami7@gmail.com>
2024-05-31 17:39:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ function padRow(rowNumber, rowCount) {
   rows.push(padRow(rows.length + 1, count));
 }*/
 
+/*for (let i = count; i > 0; i--) {
+  rows.push(padRow(i, count));
+}*/
+
 
 
 let result = ""
